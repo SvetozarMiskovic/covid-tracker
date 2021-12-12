7dabaec38105b47df619b2110deaaf92ec735c9c
SvetozarMiskovic <svetomiskovic98@gmail.com>
2021-12-12 16:54:12 +0100

    Finished!

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,13 +1,23 @@
 import React from 'react';
-
+import '../styles/Information.css';
 function Information() {
   return (
     <div className="holder">
-      <h2 className="country-display">PLEASE SELECT A COUNTRY</h2>
+      <h2 className="country-display">PLEASE WRITE A COUNTRY</h2>
       <div className="information-box">
-        <div className="death-cases">PLEASE SELECT A COUNTRY!</div>
-        <div className="active-cases">PLEASE SELECT A COUNTRY!</div>
-        <div className="confirmed-cases">PLEASE SELECT A COUNTRY!</div>
+        <div className="confirmed">
+          <h2>Number of confirmed cases</h2>
+          <h4 className="confirmed-cases">0</h4>
+        </div>
+
+        <div className="active">
+          <h2>Number of active cases</h2>
+          <h4 className="active-cases">0</h4>
+        </div>
+        <div className="death">
+          <h2>Number of death cases</h2>
+          <h4 className="death-cases">0</h4>
+        </div>
       </div>
     </div>
   );
